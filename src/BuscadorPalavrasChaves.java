9c8291ee6feaac1f93b4c2d63cdf24ff24f0a6a9
Sayymon <sayymon@hotmail.com>
2020-10-11 17:29:44 -0300

    Incluindo o nome do Vitor na lista de paçlavras

--- src/BuscadorPalavrasChaves.java
+++ src/BuscadorPalavrasChaves.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class BuscadorPalavrasChaves {
-	private static List<String> palavras = Arrays.asList("KELLY","SAYMON","GABRIELLY","TESTE","INGLES","FLOR","CASA","CARRO");
+	private static List<String> palavras = Arrays.asList("VITOR","KELLY","SAYMON","GABRIELLY","TESTE","INGLES","FLOR","CASA","CARRO");
 	
 	public static List<String> buscarListaPalavrasForca(){
 		return palavras;
